7812796215e9194718f25f9be81cc5bdddaed76b
hujianhong <1043244432@qq.com>
2017-02-04 22:24:03 +0800

    添加发表博文的界面

---
+++ qingcai-back/static/js/api.js
@@ -0,0 +1,250 @@
+/**
+ * 青菜萝卜 API
+ * 所有异步请求的地址
+ */
+
+layui.define(['laytpl', 'laypage', 'layer','qingtpl'],function(exports) {
+	var $ = layui.jquery;
+	var laypage = layui.laypage;
+	var layer = layui.layer;
+	var laytpl = layui.laytpl;
+	var qingtpl = layui.qingtpl;
+	
+//	var BASE_PREFIX = "http://www.huding.name";
+	
+	var BASE_PREFIX = "http://localhost";
+
+	var pageSize = 10;
+	
+	var api = {
+		/**
+		 * 博文列表请求地址
+		 */
+		BLOG_DISPLAY_URL : BASE_PREFIX + "/luobo/showBlogs",
+		/**
+		 * 博文标签请求地址
+		 */
+		TAGS_DISPLAY_URL : BASE_PREFIX + "/luobo/showTags",
+		/**
+		 * 博文归档地址
+		 */
+		BLOG_TIMELINE_URL: BASE_PREFIX + "/luobo/blog/timeline",
+		/**
+		 * 热门排行地址
+		 */
+		HOT_RANK_URL: BASE_PREFIX + "/luobo/blog/hotRank",
+		/**
+		 * 猜你喜欢地址
+		 */
+		BLOG_RECOMMEND_URL: BASE_PREFIX + "/luobo/blog/recommend",
+		/**
+		 * 博客轮播地址
+		 */
+		BLOG_LUNBO_URL: BASE_PREFIX + "/luobo/blog/lunbo",
+		/**
+		 * 
+		 */
+		BLOG_TAGS_URL: BASE_PREFIX + "/luobo/blog/blogTags",
+		/**
+		 * 博文评论地址
+		 */
+		BLOG_COMMENT_URL: BASE_PREFIX + "/luobo/comment/show",
+		/**
+		 * 
+		 */
+		COMMENT_LIKE_URL: BASE_PREFIX + "/luobo/comment/like",
+		/**
+		 * 
+		 */
+		COMMENT_HATE_URL: BASE_PREFIX + "/luobo/comment/hate",
+		/**
+		 * 
+		 */
+		COMMENT_REPORT_URL: BASE_PREFIX + "/luobo/comment/report",
+		/**
+		 * 
+		 */
+		UPLOAD_IMG_URL : BASE_PREFIX + "/luobo/upload/uploadImage",
+	}
+	
+	var action = {
+		//Ajax
+		doAjax: function(url, data, success, options) {
+			var that = this;
+			options = options || {};
+			data = data || {};
+			return $.ajax({
+				type: options.type || 'post',
+				dataType: options.dataType || 'json',
+				data: data,
+				url: url,
+				success: function(res) {
+					console.log(res);
+					if(res.code == 0) {
+						success && success(res);
+					} else {
+						layer.msg(res.msg || res.code, {
+							shift: 6
+						});
+					}
+				},
+				error: function(e) {
+					options.error || layer.msg('请求异常，请重试', {
+						shift: 6
+					});
+				}
+			});
+		},
+		
+		blogLunbo:function(params,success){
+			action.doAjax(api.BLOG_LUNBO_URL,params,success);
+		},
+		/*
+		 * 请求博文标签云数据
+		 */
+		blogTagCloud:function(params,success) {
+			action.doAjax(api.BLOG_TAGS_URL,params,success);
+		},
+		qingComment:function(params,success) {
+			action.doAjax(api.BLOG_COMMENT_URL,params,success);
+		},
+		commentLike:function(params,success){
+			action.doAjax(api.COMMENT_LIKE_URL,params,success);
+		},
+		uploadImage:function(params,success){
+			layui.use('upload', function(){
+				layui.upload({
+					url: api.UPLOAD_IMG_URL,
+					success: success
+				});
+			});
+		},
+		reportComment:function(params,success){
+			action.doAjax(api.COMMENT_REPORT_URL,params,success);
+		},
+		
+		
+		/* 待修改函数 */
+		loadBlogTags : function (){
+			action.doAjax(api.TAGS_DISPLAY_URL,{},function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.blogTagTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#blog-tags").html(html);
+				});
+			});
+		},
+		
+		loadHotestBlogs : function (){
+			action.doAjax(api.BLOG_DISPLAY_URL,{},function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.hotestBlogsTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#hotest-blogs").html(html);
+				});
+			});
+		},
+		loadHotRankBlogs:function(){
+			var params = {
+				pageNum:1,
+				pageSize:5
+			};
+			action.doAjax(api.HOT_RANK_URL,params,function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.hotRankTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#hot-rank-blogs").html(html);
+				});
+			});
+		},
+		loadRecommedBlogs:function(){
+			var params = {
+				pageNum:1,
+				pageSize:5
+			};
+			action.doAjax(api.BLOG_RECOMMEND_URL,params,function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.recommendBlogsTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#recommend-blogs").html(html);
+				});
+			});
+		},
+		loadBlogTimeline:function(){
+			action.doAjax(api.BLOG_TIMELINE_URL,{},function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.blogTimelineTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#blog-timeline").html(html);
+				});
+			});
+		},
+		loadLastestBlogs : function (){
+			action.doAjax(api.BLOG_DISPLAY_URL,{},function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.latestBlogsTpl);
+				// 渲染数据
+				tpl.render(result,function(html){
+					// 显示内容
+					$("#lastest-blogs").html(html);
+				});
+			});
+		},
+		pageCallback : function(pageNum,pageSize){
+			var params = {
+				pageNum:pageNum,
+				pageSize:pageSize
+			};
+			action.doAjax(api.BLOG_DISPLAY_URL,params,function(result){
+				// 加载模板
+				//var tpl = laytpl($("#blog-list-template").html());
+				var tpl = laytpl(qingtpl.blogListTpl);
+				// 渲染数据
+				tpl.render(result.data,function(html){
+					// 显示内容
+					$("#blog-list").html(html);
+				});
+				// 回到顶部
+				var speed=200;//滑动的速度
+                $('body').animate({ scrollTop: 0 }, speed);
+			});
+		},
+		loadBlogs : function(){
+			var params = {
+				pageNum:1,
+				pageSize:pageSize
+			};
+			action.doAjax(api.BLOG_DISPLAY_URL,params,function(result){
+				// 加载模板
+				var tpl = laytpl(qingtpl.blogListTpl);
+				// 渲染数据
+				tpl.render(result.data,function(html){
+					// 显示内容
+					$("#blog-list").html(html);
+				});
+				// 调用分页
+				laypage({
+					cont: 'blog-pager',
+					pages: result.data.totalPage, //得到总页数
+					jump: function(conf,first) {
+						if(first){
+							return;
+						}
+						action.pageCallback(conf.curr,pageSize);
+					}
+				});
+			});
+		},
+	}
+	exports('api', action);
+});
---
+++ qingcai-back/test.md
@@ -0,0 +1,152 @@
+# Heading 1
+
+### 字符效果和横线等
+                
+----
+
+~~删除线~~ <s>删除线（开启识别HTML标签时）</s>
+*斜体字*      _斜体字_
+**粗体**  __粗体__
+***粗斜体*** ___粗斜体___
+
+上标：X<sub>2</sub>，下标：O<sup>2</sup>
+
+**缩写(同HTML的abbr标签)**
+
+> 即更长的单词或短语的缩写形式，前提是开启识别HTML标签时，已默认开启
+
+The <abbr title="Hyper Text Markup Language">HTML</abbr> specification is maintained by the <abbr title="World Wide Web Consortium">W3C</abbr>.
+
+### 引用 Blockquotes
+
+> 引用文本 Blockquotes
+
+引用的行内混合 Blockquotes
+                    
+> 引用：如果想要插入空白换行`即<br />标签`，在插入处先键入两个以上的空格然后回车即可，[普通链接](http://localhost/)。
+
+### 锚点与链接 Links
+
+[普通链接](http://localhost/)
+
+[普通链接带标题](http://localhost/ "普通链接带标题")
+
+直接链接：<https://github.com>
+
+[锚点链接][anchor-id] 
+
+[anchor-id]: http://www.this-anchor-link.com/
+
+[mailto:test.test@gmail.com](mailto:test.test@gmail.com)
+
+GFM a-tail link @pandao  邮箱地址自动链接 test.test@gmail.com  www@vip.qq.com
+
+> @pandao
+
+### 多语言代码高亮 Codes
+
+#### 行内代码 Inline code
+
+执行命令：`npm install marked`
+
+#### 缩进风格
+
+即缩进四个空格，也做为实现类似 `<pre>` 预格式化文本 ( Preformatted Text ) 的功能。
+
+    <?php
+        echo "Hello world!";
+    ?>
+    
+预格式化文本：
+
+    | First Header  | Second Header |
+    | ------------- | ------------- |
+    | Content Cell  | Content Cell  |
+    | Content Cell  | Content Cell  |
+
+
+#### HTML 代码 HTML codes
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <mate charest="utf-8" />
+        <meta name="keywords" content="Editor.md, Markdown, Editor" />
+        <title>Hello world!</title>
+        <style type="text/css">
+            body{font-size:14px;color:#444;font-family: "Microsoft Yahei", Tahoma, "Hiragino Sans GB", Arial;background:#fff;}
+            ul{list-style: none;}
+            img{border:none;vertical-align: middle;}
+        </style>
+    </head>
+    <body>
+        <h1 class="text-xxl">Hello world!</h1>
+        <p class="text-green">Plain text</p>
+    </body>
+</html>
+```
+
+### 图片 Images
+
+![](https://pandao.github.io/editor.md/examples/images/8.jpg)
+
+> 图为：厦门白城沙滩
+
+
+
+#### GFM task list
+
+- [x] GFM task list 1
+- [x] GFM task list 2
+- [ ] GFM task list 3
+    - [ ] GFM task list 3-1
+    - [ ] GFM task list 3-2
+    - [ ] GFM task list 3-3
+- [ ] GFM task list 4
+    - [ ] GFM task list 4-1
+    - [ ] GFM task list 4-2
+                
+----
+                    
+### 绘制表格 Tables
+
+| 项目        | 价格   |  数量  |
+| --------   | -----:  | :----:  |
+| 计算机      | $1600   |   5     |
+| 手机        |   $12   |   12   |
+| 管线        |    $1    |  234  |
+                    
+First Header  | Second Header
+------------- | -------------
+Content Cell  | Content Cell
+Content Cell  | Content Cell 
+
+| First Header  | Second Header |
+| ------------- | ------------- |
+| Content Cell  | Content Cell  |
+| Content Cell  | Content Cell  |
+
+| Function name | Description                    |
+| ------------- | ------------------------------ |
+| `help()`      | Display the help window.       |
+| `destroy()`   | **Destroy your computer!**     |
+
+| Left-Aligned  | Center Aligned  | Right Aligned |
+| :------------ |:---------------:| -----:|
+| col 3 is      | some wordy text | $1600 |
+| col 2 is      | centered        |   $12 |
+| zebra stripes | are neat        |    $1 |
+
+| Item      | Value |
+| --------- | -----:|
+| Computer  | $1600 |
+| Phone     |   $12 |
+| Pipe      |    $1 |
+                
+----
+
+ 
+#### 反斜杠 Escape
+
+\*literal asterisks\*
